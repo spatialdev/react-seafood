5c962ba28d7864e97f1f123ca002f01141db9c98
Daniel Baah <danielduhh@gmail.com>
2018-06-08 09:37:46 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import { Map } from './Map.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = {
+        open: true,
+        anchor: 'left',
+    };
+
+  render() {
+
+      const { anchor, open } = this.state;
+
+      return (
+      <div className="App">
+          <Drawer
+              variant="persistent"
+              anchor={anchor}
+              open={open}
+          >
+              cool stuff in here
+          </Drawer>
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className="Map">
+            <Map></Map>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
