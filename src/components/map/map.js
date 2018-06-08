94e430770995a1e8d14cf31889f0b35f16cb8206
Daniel Baah <danielduhh@gmail.com>
2018-06-08 13:06:53 -0700

    full size map

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
+import windowSize from 'react-window-size';
+import './map.css';
 
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3BhdGlhbGRldiIsImEiOiJKRGYyYUlRIn0.PuYcbpuC38WO6D1r7xdMdA';
 
-export class Map extends Component {
+class Map extends Component {
 
     state = {
         viewport: {
@@ -11,18 +13,28 @@ export class Map extends Component {
             longitude: -100,
             zoom: 3,
             bearing: 0,
-            pitch: 0,
-            width: 500,
-            height: 500
+            pitch: 0
         }
     };
 
     render() {
+
+        this.state.viewport =
+            {
+                ...this.state.viewport,
+                width: this.props.windowWidth,
+                height: this.props.windowHeight
+        }
+
         return (
-            <ReactMapGL mapboxApiAccessToken={MAPBOX_TOKEN}
-                {...this.state.viewport}
-                onViewportChange={(viewport) => this.setState({viewport})}
-            />
+            <div className="Map">
+                <ReactMapGL mapboxApiAccessToken={MAPBOX_TOKEN}
+                        {...this.state.viewport}
+                        onViewportChange={(viewport) => this.setState({viewport})}
+                />
+            </div>
         );
     }
 }
+
+export default windowSize(Map);
