2b868178a680bf06729b062aae8bee8b7a0bb84b
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-06-08 16:20:49 -0700

    Get menu contents

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import Map from './components/map/map.js';
 import PersistentDrawer from './components/leftMenuTopNav/leftMenu';
 import './app.css';
+import polygons from './data/polygons_2017.geojson';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     clickedMenuItem: null,
+    polygonData: null,
   };
+
+  componentDidMount() {
+    return axios.get(polygons)
+      .then((res) => {
+        this.setState({ polygonData: res.data });
+      });
+  }
 
   handleMenuData = (itemId) => {
     this.setState({ clickedMenuItem: itemId }, () => {
@@ -15,10 +25,13 @@ class App extends Component {
   };
 
   render() {
-
+    const { polygonData } = this.state;
+    if (!polygonData) {
+      return <div>Loading...</div>;
+    }
     return (
       <div className="App">
-        <PersistentDrawer clickedMenuItem={this.handleMenuData}/>
+        <PersistentDrawer clickedMenuItem={this.handleMenuData} polygonData={polygonData}/>
         <Map/>
       </div>
     );
