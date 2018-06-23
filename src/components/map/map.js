0e65a97b8bc81d423dd18b29ecd9137f69ccac31
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-06-22 18:44:48 -0700

    Add geolocate button

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import mapboxgl from 'mapbox-gl'
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { inside, point, polygon } from '@turf/turf';
 import windowSize from 'react-window-size';
 import './map.css';
 
@@ -16,15 +17,16 @@ class Map extends Component {
       zoom: 18,
       bearing: 0,
       pitch: 0
-    }
+    },
+    trackUserLocation: true,
   };
 
   // TODO
   bounds = [
-    //s outhwest
-    [ -122.38885402679443, 47.66567637286265],
+    //southwest
+    [-122.38885402679443, 47.66567637286265],
     //northeast
-    [ -122.37951993942261, 47.6712685277693]
+    [-122.37951993942261, 47.6712685277693]
   ];
 
   componentDidMount() {
@@ -33,42 +35,58 @@ class Map extends Component {
       container: this.mapContainer,
       style: 'mapbox://styles/spatialdev/cj44jbnm59nnq2rmrzd5ozf36',
       center: [-122.38473415374757, 47.668667600018416],
-      maxBounds: this.bounds,
+      // maxBounds: this.bounds,
       zoom: 18
     });
 
-    this.map.on('load', () => {});
+    const geoLocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: this.state.trackUserLocation,
+    });
+
+    this.map.addControl(geoLocate);
+
+    geoLocate.on('geolocate', (e) => {
+      this.handleGeolocation(e);
+    });
+
+    this.map.on('load', () => {
+    });
 
     this.map.on('click', (e) => {
 
       // Fetch map feature from specified layer list.
       // TODO grab this layer list from a configuration
-      let features = this.map.queryRenderedFeatures(e.point, {layers: [
+      let features = this.map.queryRenderedFeatures(e.point, {
+        layers: [
           'vendor icons',
           'vendor pins',
           'centroid labels',
           'game icons',
           'entertainment polygons',
           'vendor pins highlight'
-        ]});
+        ]
+      });
 
       if (features.length > 0) {
-        this.handleMapPopup(e, features)
+        this.handleMapPopup(e, features);
       }
-    })
+    });
   }
 
   render() {
 
     return (
       <div className="Map">
-        <div ref={el => this.mapContainer = el} className="GL-Map" />
+        <div ref={el => this.mapContainer = el} className="GL-Map"/>
       </div>
     );
   }
 
   // Render map pop up on click
-  handleMapPopup (e, features) {
+  handleMapPopup(e, features) {
     const coordinates = e.lngLat;
     const html = this.createPopupHTML(features[0].properties);
 
@@ -86,8 +104,41 @@ class Map extends Component {
             ${properties.type}
         </p>
 
-       `
+       `;
   }
+
+  handleGeolocation = (position) => {
+    if ('geolocation' in navigator) {
+      /* geolocation is available */
+
+      navigator.geolocation.getCurrentPosition(position => {
+
+        let marker = (<div id="Dot"></div>);
+
+        let myMarkerLocation = new mapboxgl.Marker(marker)
+          .setLngLat([position.coords.longitude, position.coords.latitude])
+          .addTo(this.map);
+      });
+
+      // check if position is within the bounds of the map
+      let pt = point([position.coords.longitude, position.coords.latitude]);
+      let poly = polygon([[[-122.3892831802368, 47.66561856968346], [-122.37885475158693, 47.66561856968346], [-122.37885475158693, 47.671889841329026], [-122.3892831802368, 47.671889841329026], [-122.3892831802368, 47.66561856968346]]]);
+
+      let isInside = inside(pt, poly);
+
+      if (isInside) {
+        this.map.flyTo({
+          center: [position.coords.longitude, position.coords.latitude],
+          zoom: 19.7
+        });
+      } else {
+        alert('Location is outside Seafood Fest bounds. Try again.');
+        this.setState({ trackUserLocation: false });
+      }
+    } else {
+      alert('Location information is unavailable.');
+    }
+  };
 }
 
 export default windowSize(Map);
